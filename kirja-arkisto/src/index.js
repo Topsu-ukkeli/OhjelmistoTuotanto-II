c119400309da4dd43dfede52799b9a484ce3caa3
Topsu-ukkeli <topi.leinonen@edu.savonia.fi>
2023-02-10 13:28:07 +0200

    Säädin lisää :)

--- kirja-arkisto/src/index.js
+++ kirja-arkisto/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Arkisto from './Kirja-arkisto'
-
+// import App from './App';
+import {Arkisto} from './Kirja-arkisto';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
     <Arkisto />
   </React.StrictMode>
 );
