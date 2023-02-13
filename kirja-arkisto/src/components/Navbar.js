b1cb6f32c93b8f27290f7e4ee044e80a88d1c8ab
Ossi <ossmie@outlook.com>
2023-02-13 18:30:26 +0200

    kaikki rikki

--- kirja-arkisto/src/components/Navbar.js
+++ kirja-arkisto/src/components/Navbar.js
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom'
 import './Navbar.css';
+import logo from '../images/logo.png'
 
 export const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="navbar-logo">
-
+            <img src={logo} alt="Kirjasto logo" />
             </div>
             <ul className="navbar-links">
                 <li className="nav-item">
