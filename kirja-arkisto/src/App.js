82a9f351ab21fd09de71ac2e4880fb6f218b9ede
Ossi <ossmie@outlook.com>
2023-02-15 19:05:30 +0200

    rikiki

--- kirja-arkisto/src/App.js
+++ kirja-arkisto/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './App.css';
 import { Navbar } from "./components/Navbar";
 import { Home } from './components/Home';
@@ -13,10 +13,10 @@ function App() {
     <div className="App">
       <Router>
         <Navbar />
-        <Routes>
-          <Route exact path='/' element={<Home />}/>
-          <Route exact path='/oma-kirjasto' element={<OmaKirjasto />}/>
-        </Routes>
+        <Switch>
+          <Route path="/"><Home /></Route>
+          <Route path="/oma-kirjasto"> <OmaKirjasto/></Route>
+        </Switch>
       </Router>
     </div>
   );
