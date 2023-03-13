497cc4aa9e06cff4e37739d54d9f652e6d674848
Ossi <ossmie@outlook.com>
2023-03-13 13:11:25 +0200

    hämmennystä

--- kirja-arkisto/src/components/Home.js
+++ kirja-arkisto/src/components/Home.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React from 'react';
 import "./Home.css";
 
+
 export const Home = () => {
+
+
 	return (
 		<div className="home-container">
-			<h1>Etusivu tähän</h1>
+			<h1>Databaseen jotain postauksia ja ne renderöidään sitten tässä</h1>
 		</div>
 	);
 };
