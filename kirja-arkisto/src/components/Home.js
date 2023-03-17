cf5bfc49085d65264845b2c367baf05d9a6666e1
Kumpulainen1 <95127870+Kumpulainen1@users.noreply.github.com>
2023-03-17 14:19:22 +0200

    Frontti update

--- kirja-arkisto/src/components/Home.js
+++ kirja-arkisto/src/components/Home.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import "./Home.css";
+import kirja from '../images/kirja.jpg'
 
 
 export const Home = () => {
-
-
 	return (
-		<div className="home-container">
-			<h1>Databaseen jotain postauksia ja ne renderöidään sitten tässä</h1>
+		<div className='wrapper'>
+			<Card
+				title="harry potter ja viisasten transporter"
+				desc="Kirja kertoo siitä kuinka harry osti ruosteisen transporterin" />
+			<Card
+				title="Longin levitys 105"
+				desc="Teppo heittää maidon longi ovilta ja mikko piikka ja muuta pelaa anti valoo" />
+			<Card
+				title="Kirja tässä tosiaan"
+				desc="JÖÖ KIRJAA RÖH KÖH" />
 		</div>
 	);
 };
+
+function Card(props) {
+	return (
+		<div className='card'>
+			<div className='cardbody'>
+				{/* muokataan kuvan tuontia vielä. tämä on vain esimerkki */}
+				<img src={kirja} alt="kirja" className='card_img'></img>
+				<h2 className='cardtitle'>{props.title}</h2>
+				<p className='carddesc'>{props.desc}</p>
+			</div>
+			<button className='cardbtn' >Lisätiedot</button>
+		</div>
+	)
+}
