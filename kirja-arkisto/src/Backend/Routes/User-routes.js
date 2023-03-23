c2a65dd34d6dcca1bc1fb2067f843ee794c1c3f5
Topsu-ukkeli <topi.leinonen@edu.savonia.fi>
2023-03-23 19:18:24 +0200

    Backend muuten valmis kuvan tallennus uupuu

--- kirja-arkisto/src/Backend/Routes/User-routes.js
+++ kirja-arkisto/src/Backend/Routes/User-routes.js
@@ -5,5 +5,7 @@ const UserControllers = require('../controllers/User-controllers');
 const router = express.Router();
 
 router.post('/createusers/', UserControllers.createdUser);
+router.delete('/:_id',UserControllers.DeleteUser);
+router.patch('/:_id',UserControllers.updateUserById);
 router.get('/',UserControllers.getAllUsers);
 module.exports = router;
