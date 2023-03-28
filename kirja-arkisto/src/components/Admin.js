f573d0d460c02d0e1224d59b63d4802139b26eb4
Topsu-ukkeli <topi.leinonen@edu.savonia.fi>
2023-03-28 15:08:00 +0300

    Odotan hyväksyntää kirjojen/sarjan muokkausta

--- kirja-arkisto/src/components/Admin.js
+++ kirja-arkisto/src/components/Admin.js
@@ -8,7 +8,19 @@ const Admin = () => {
     const [kirjat, setKirjat] = useState([]);
     const [error, setError] = useState(null);
     const [visible, setVisible] = useState(false);
-
+    const [kirjaMuokkausVisible, setKirjaMuokkausVisible] = useState(false);
+    const [sarjaMuokkausVisible, setSarjaMuokkausVisible] = useState(false);
+    const [kirjaMuokkaus, setKirjaMuokkaus] = useState([]);
+    const [sarjatMuokkausT, setSarjatMuokkausT] = useState([]);
+    const [sarjatMuokkaus, setSarjatMuokkaus] = useState("0");
+    const [Luokittelu, setLuokittelu] = useState("");
+    const numbers = [
+        {id:1,number:1},
+        {id:2,number:2},
+        {id:3,number:3},
+        {id:4,number:4},
+        {id:5,number:5}
+    ]
     useEffect(() => {
         fetchUsers();
     }, [kirjat])
@@ -23,14 +35,14 @@ const Admin = () => {
         }
     }
     const DeleteKirja = async (kirja) => { // tässä tarkistus varmistaakseen että käyttäjä haluaa poistaa kirjan
-        if(window.confirm("Haluatko varmasti poistaa kirjan?")){
-        await fetch(
-            `http://localhost:5000/api/kirja/deletekirja/${kirja._id}`,
-            {
-                method: "DELETE",
-            }
-        );
-    }
+        if (window.confirm("Haluatko varmasti poistaa kirjan?")) {
+            await fetch(
+                `http://localhost:5000/api/kirja/deletekirja/${kirja._id}`,
+                {
+                    method: "DELETE",
+                }
+            );
+        }
     };
 
     const [sarjat, setSarjat] = useState([]);
@@ -48,62 +60,56 @@ const Admin = () => {
         }
     }
     const DeleteSarja = async (sarja) => { // tässä tarkistus varmistaakseen että käyttäjä haluaa poistaa sarjan
-        if(window.confirm("Haluatko varmasti poistaa sarjan?")){
-        await fetch(
-            `http://localhost:5000/api/sarja/${sarja._id}`,
-            {
-                method: "DELETE",
-            }
-        );
+        if (window.confirm("Haluatko varmasti poistaa sarjan?")) {
+            await fetch(
+                `http://localhost:5000/api/sarja/${sarja._id}`,
+                {
+                    method: "DELETE",
+                }
+            );
         }
     };
 
     const handleToggle = () => {
+        setKirjaMuokkausVisible(false);
+        setSarjaMuokkausVisible(false);
         setVisible((current) => !current);
     }
+    const handleKirjaMuokkaus = ({ kirja }) => {
+        setKirjaMuokkausVisible(true);
+        setVisible((current) => !current);
+        setKirjaMuokkaus(kirja);
+    }
+    const handleSarjaMuokkaus = ({ sarja }) => {
+        setSarjaMuokkausVisible(true);
+        setVisible((current) => !current);
+        setSarjatMuokkausT(sarja)
+    }
+    const PaivitaSarja = () => {
+        
+    }
+    const PaivitaKirja = () => {
 
-    // function Tallenna() {
-    //     const newBook = {
-    //         title: title,
-    //         author: author,
-    //         published: published,
-    //         page: page,
-    //         image: image,
-    //         sarjaid: sarjaid
-    //     };
-    //     console.log("tähän tulee uusi kirja", newBook)
-    //     fetch("http://localhost:5000/api/Kirja/Kirjat", {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify(newBook)
-    //     })
-    //         .then(response => response.json())
-    //         .then(data => console.log(data))
-    //         .catch(error => console.error(error));
-    //         setShowmsg(true);
-    // }
-
-
+    }
     return (
         <div>
             <Link to="/Kirjalisaus">
                 <button class="button-85" role="button" >Lisää kirja tietokantaan</button>
             </Link>
             <br />
-            <br/>
-            <Link to="/Sarjalisaus">
-                <button class="button-85" role="button">Lisää uusi sarja tietokantaan</button>
-            </Link>
-            <br/>
-            <br/>
-            <button class="button-85" role="button" onClick={() => handleToggle()}>Poista kirja tai sarja tietokannasta</button>
             <br />
-            <br/>
-            <Link to="/Kirjamuokkaus">
-            <button class="button-85" role="button">Muokkaa kirjaa</button>
+            <Link to="/Sarjalisaus">
+                <button class="button-85" role="button" >Lisää uusi sarja tietokantaan</button>
             </Link>
+            <br />
+            <br />
+            <button class="button-85" role="button" onClick={() => handleToggle()}>Poista kirja tai sarja tai muokkaa tietoja</button>
+            <br />
+            <br />
+            {/* <Link to="/Kirjamuokkaus">
+            </Link> */}
+            {/* mahdollisesti poistetaan tuo Link ja laitetaan vaan tälle sivulle se muokkaus tuolla visible tyylillä */}
+
             {visible && (
                 <div className="sarja-admin">
                     <h1>Sarjat:</h1>
@@ -126,7 +132,8 @@ const Admin = () => {
                                 {sarja.Luokittelu}
                             </td>
                             <td>
-                                <button onClick={() => DeleteSarja(sarja)}>Poista sarja</button>
+                                <button class="button-85" onClick={() => DeleteSarja(sarja)}>Poista sarja</button>
+                                <button class="button-85" onClick={() => handleSarjaMuokkaus({ sarja })}>Muokkaa sarjaa</button>
                             </td>
                         </tr>
                     ))}
@@ -151,14 +158,91 @@ const Admin = () => {
                                 {kirja.published}
                             </td>
                             <td>
-                                <button onClick={() => DeleteKirja(kirja)}>Poista kirja </button>
+                                <button class="button-85" onClick={() => DeleteKirja(kirja)}>Poista kirja </button>
+                                <button class="button-85" onClick={() => handleKirjaMuokkaus({ kirja })}>Muokkaa kirjaa</button>
                             </td>
                         </tr>
                     ))}
                 </div>
             )}
+            {kirjaMuokkausVisible && (
+                <div className="kirja-admin">
+                    <label>
+                        Kirjan nimi:
+                        <input type="text" value={kirjaMuokkaus.title}></input>
+                    </label>
+                    <label>
+                        Kirjoittaja:
+                        <input type="text" value={kirjaMuokkaus.author}></input>
+                    </label>
+                    <label>
+                        Julkaisuvuosi:
+                        <input type="text" value={kirjaMuokkaus.published}></input>
+                    </label>
+                    <label>
+                        Sivumäärä:
+                        <input type="text" value={kirjaMuokkaus.page}></input>
+                    </label>
+                    <label className='labels'>
+                        SeriesID:
+                        <select className='kirja-input' onChange={(e) => setSarjatMuokkaus(e.target.value)}>
+                            {sarjat.map((sarja, index) => (
+                                <option key={`${sarja.sarjaid}_${index}`} value={sarja.sarjaid}>{sarja.Sarjanimi}</option>
+                            ))}
+                        </select>
+                    </label>
+                    <button onClick={PaivitaKirja}>Päivitä tiedot</button>
+                </div>
+            )}
+            {sarjaMuokkausVisible && (
+                <div className="kirja-admin">
+                    <label>
+                        Sarjanimi:
+                        <input type="text" value={sarjatMuokkausT.Sarjanimi}></input>
+                    </label>
+                    <label>
+                        Kustantaja:
+                        <input type="text" value={sarjatMuokkausT.Kustantaja}></input>
+                    </label>
+                    <label>
+                        Kuvaus:
+                        <input type="text" value={sarjatMuokkausT.Kuvaus}></input>
+                    </label>
+                    <label className='labels'>
+                        Luokittelu:
+                        <select onChange={(e) => setLuokittelu(e.target.value)}>
+                            {numbers.map((row) => (
+                            <option key={row.id} value = {row.number}>{row.number}</option>
+                            ))}
+                        </select>
+                    </label>
+                    <button onClick={PaivitaSarja}>Päivitä tiedot</button>
+                </div>
+            )}
         </div>
     )
 }
+// function Tallenna() {
+//     const newBook = {
+//         title: title,
+//         author: author,
+//         published: published,
+//         page: page,
+//         image: image,
+//         sarjaid: sarjaid
+//     };
+//     console.log("tähän tulee uusi kirja", newBook)
+//     fetch("http://localhost:5000/api/Kirja/Kirjat", {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(newBook)
+//     })
+//         .then(response => response.json())
+//         .then(data => console.log(data))
+//         .catch(error => console.error(error));
+//         setShowmsg(true);
+// }
 
 export { Admin }
--- kirja-arkisto/src/components/Kirjamuokkaus.js
+++ kirja-arkisto/src/components/Kirjamuokkaus.js
@@ -1,5 +1,7 @@
-const Kirjamuokkaus = () => {
+const Kirjamuokkaus = ({kirjat,sarjat}) => {
 
+    console.log("kirjat on",kirjat);
+    console.log("kirjat on",sarjat);
     return(
         <div>
             <h1>terve</h1>
